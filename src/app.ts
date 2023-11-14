6b4084f042e417090a671fe9ebc3f98973c0877b
Kingston Duffie <kduffie@hivepoint.com>
2023-11-14 10:22:57 -0800

    Basic HTML for home page

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,22 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.contentType('text/html');
+  const html = `
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Chess</title>
+  </head>
+  <body>
+    <h1>Chess</h1>
+    <p>
+      <button>Go!</button>
+    </p>
+  </body>
+</html>
+  `;
+  res.send(html);
 });
 
 app.listen(port, () => {
